f1a374cb3fb697479f8bd4a3e9610f6de4c2e5a8
Michael Kessler <michi@netzpiraten.ch>
2012-08-14 21:41:05 +0200

    Add admin user.

---
+++ app/admin/admin_users.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register AdminUser do
+  index do
+    column :email
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    default_actions
+  end
+
+  form do |f|
+    f.inputs 'Admin Details' do
+      f.input :email
+
+      if !f.object.new_record?
+        f.input :password, required: false
+        f.input :password_confirmation, required: false
+      end
+
+      f.form_buffers.last
+    end
+    f.buttons
+  end
+end
---
+++ app/models/admin_user.rb
@@ -0,0 +1,27 @@
+class AdminUser < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+
+  after_create do |admin|
+    admin.send_reset_password_instructions
+  end
+
+  before_destroy :raise_if_last
+
+  def raise_if_last
+    if AdminUser.count < 2
+      raise "Can't delete last admin user"
+    end
+  end
+
+  def password_required?
+    false
+  end
+end
