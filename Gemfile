8e9b7afd32e892f5b1fcbba3cdb0c7206d7f5aa1
Michael Kessler <michi@netzpiraten.ch>
2012-08-14 19:32:09 +0200

    Initial app.

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+gem 'pg'
+
+gem 'activeadmin'
+gem 'meta_search',    '>= 1.1.0.pre'
+
+gem 'compass'
+gem 'haml'
+gem 'haml-rails'
+
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VareseCorridor::Application.config.secret_token = 'b81d22e03dae0cc7d05655fc1172ada7b693f4b8c083e60c232fa5f034fa71d6621ed2e52b1e87d43beedf813f58349e3f851a3ddb27cce09bd80205ca523286'
