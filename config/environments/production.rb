3636470afdfd1254d4c0c84117e69f6867795edb
Michael Kessler <michi@netzpiraten.ch>
2012-08-16 20:14:59 +0200

    Add mailer host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,8 @@ VareseCorridor::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'www.varese-corridor.ch' }
+
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
