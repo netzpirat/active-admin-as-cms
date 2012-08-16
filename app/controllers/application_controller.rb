39006532f4ae27d49f5eb6e62df88642a32dcd95
Michael Kessler <michi@netzpiraten.ch>
2012-08-16 18:21:09 +0200

    Fix language detection.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :check_locale
+
+  def check_locale
+    I18n.locale = params[:locale] if params[:locale]
+    I18n.locale = I18n.default_locale unless I18n.available_locales.include?(I18n.locale)
+  end
 end
